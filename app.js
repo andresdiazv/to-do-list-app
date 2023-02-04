227f3477754ecc01697119334e9fc75aef4eb1f3
andresdiazv <adiaz616@fiu.edu>
2023-02-04 01:14:23 -0500

    removed repition and cleaned code up

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js");
 
 const app = express();
 
-let items = ["Buy Food", "Cook Food", "Eat Food"];
-let workItems = [];
+const items = ["Buy Food", "Cook Food", "Eat Food"];
+const workItems = [];
 
 app.set("view engine", "ejs");
 
@@ -12,11 +13,12 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
+  const day = date.getDate();
   res.render("list", { listTitle: day, newListItems: items });
 });
 
 app.post("/", (req, res) => {
-  let item = req.body.newItem;
+  const item = req.body.newItem;
 
   if (req.body.list === "Work") {
     workItems.push(item);
@@ -32,15 +34,14 @@ app.get("/work", (req, res) => {
 });
 
 app.post("/work", (req, res) => {
-  let item = req.body.newItem;
+  const item = req.body.newItem;
   workItems.push(item);
   res.redirect("/work");
 });
 
 app.get("/about", (req, res) => {
-    res.render("about");
+  res.render("about");
 });
-
 
 app.listen(3000, () => {
   console.log("Server is up on port 3000");
--- date.js
+++ date.js
@@ -1,16 +1,21 @@
+exports.getDate = function () {
+  const today = new Date();
 
+  const options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long",
+  };
 
-
-function getDate() {
-let today = new Date();
-
-let options = {
-  weekday: "long",
-  day: "numeric",
-  month: "long",
+  return today.toLocaleDateString("en-US", options);
 };
 
-let day = today.toLocaleDateString("en-US", options);
+exports.getDay = function () {
+  const today = new Date();
 
-return day;
-}
+  const options = {
+    weekday: "long",
+  };
+
+  return today.toLocaleDateString("en-US", options);
+};
